c5e311745dd29cb4a9d160e2158433a7a9ffd2d3
Abhijeet Singh <abhijsin@microsoft.com>
2018-04-19 23:42:39 +0530

    Adding Controllers. Refactoring code.

--- src/main/java/com/upgrad/Ch1Application.java
+++ src/main/java/com/upgrad/Ch1Application.java
@@ -1,12 +1,22 @@
 package com.upgrad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.upgrad.repository.MovieRepository;
 
 @SpringBootApplication
 public class Ch1Application {
 
+	@Autowired
+	MovieRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Ch1Application.class, args);
+	}
+	
+	public void run(String... arg0) throws Exception {
+		// clear all record if existed before do the tutorial with new data 
+		repository.deleteAll();
 	}
 }
